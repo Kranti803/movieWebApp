1749b27aff750fdc00bde0d0af7ac8815a5927d4
Kranti Kumar Shah <Krantikumar803@gmail.com>
2025-07-07 15:01:35 +0545

    changed the player

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -18,8 +18,8 @@ const Player = () => {
       {mediaType === "tv" ? (
         <>
           <iframe
-            // src={`https://player.autoembed.cc/embed/${mediaType}/${id}/${season}/${episode}?server=8`}
-            src={`https://moviesapi.club/${mediaType}/${id}-${season}-${episode}?server=8`}
+            src={`https://player.autoembed.cc/embed/${mediaType}/${id}/${season}/${episode}?server=8`}
+            // src={`https://moviesapi.club/${mediaType}/${id}-${season}-${episode}?server=8`}
             title="myTitle"
             allowFullScreen
           ></iframe>
@@ -64,8 +64,8 @@ const Player = () => {
         </>
       ) : (
         <iframe
-          // src={`https://player.autoembed.cc/embed/${mediaType}/${id}?server=8`}
-          src={`https://moviesapi.club/${mediaType}/${id}`}
+          src={`https://player.autoembed.cc/embed/${mediaType}/${id}?server=8`}
+          // src={`https://moviesapi.club/${mediaType}/${id}`}
           title="myTitle"
           allowFullScreen
         ></iframe>
